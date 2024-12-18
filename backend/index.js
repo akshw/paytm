94bb709d0dff64ee4fb49e8c639b088e8d510c58
akash <wizardmakes@gmail.com>
2024-12-18 18:19:17 +0530

    fix

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,9 @@ app.use(express.json());
 app.use(bodyparser.json());
 
 app.use("/api/v1", rootrouter);
+app.get("/", (req, res) => {
+  res.send("Home");
+});
 
 app.listen(3000, () => {
   console.log("server up on 3000");
